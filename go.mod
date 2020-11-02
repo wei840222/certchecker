7f418048614e140f041560bfd7c1f0c0965118c2
wei <wei840222@gmail.com>
2020-11-02 21:58:14 +0800

    add web

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/wei840222/certchecker
 go 1.15
 
 require (
+	github.com/gin-contrib/static v0.0.0-20200916080430-d45d9a37d28e
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
 	github.com/silenceper/gowatch v0.0.0-20201011022207-2725a3e5b521 // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/wei840222/certchecker/cert"
 	"github.com/wei840222/certchecker/handler"
 
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,7 @@ func main() {
 	go bot.HandleUpdate()
 
 	r := gin.Default()
+	r.Use(static.Serve("/", static.LocalFile("./web/dist", false)))
 	d := r.Group("/domain")
 	{
 		d.GET("", handler.ListDomain)
