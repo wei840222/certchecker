4139919092f09430f8d9a269bc2e0e582c7b1448
wei <wei840222@gmail.com>
2020-11-05 21:33:40 +0800

    ㄇasdfasgasgasfg

---
+++ conf/viper.go
@@ -0,0 +1,18 @@
+package conf
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+func init() {
+	viper.SetConfigName("config") // name of config file (without extension)
+	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("./conf") // optionally look for config in the working directory
+	// Find and read the config file
+	if err := viper.ReadInConfig(); err != nil { // Handle errors reading the config file
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+
+}
