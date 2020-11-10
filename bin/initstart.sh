510a6bb6af8a830043350493a2dd1308cd16885a
Helen <bluesky6529@gmail.com>
2020-11-10 08:11:56 +0800

    modify script path

--- bin/initstart.sh
+++ bin/initstart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /opt/helen/certchecker/web; yarn
+cd /data/helen/certchecker/web; yarn
 yarn build
-cd /opt/helen/certchecker/ ; nohup go run main.go > log/cert-`date +%Y%m%d%H%M%S`.log & 
+cd /data/helen/certchecker/ ; nohup go run main.go > log/cert-`date +%Y%m%d%H%M%S`.log & 
--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd /opt/helen/certchecker/ ; nohup go run main.go > log/cert-`date +%Y%m%d%H%M%S`.log & 
+cd /data/helen/certchecker/ ; nohup go run main.go > log/cert-`date +%Y%m%d%H%M%S`.log & 
